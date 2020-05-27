367ad8f69e0982fef5c98120a05d708365fcac94
danielmachado86 <danielmcis@gmail.com>
2020-05-27 17:06:59 -0500

    Added liveness check

---
+++ super-heroes/rest-hero/src/main/java/io/quarkus/workshop/superheroes/hero/health/PingHeroResourceHealthCheck.java
@@ -0,0 +1,27 @@
+package io.quarkus.workshop.superheroes.hero.health;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
+
+import org.eclipse.microprofile.health.HealthCheck;
+import org.eclipse.microprofile.health.HealthCheckResponse;
+import org.eclipse.microprofile.health.Liveness;
+
+import io.quarkus.workshop.superheroes.hero.HeroResource;
+
+@Liveness
+@ApplicationScoped
+public class PingHeroResourceHealthCheck implements HealthCheck {
+
+    @Inject
+    HeroResource heroResource;
+
+    @Override
+    public HealthCheckResponse call() {
+        heroResource.hello();
+        return HealthCheckResponse.named("Ping Hero REST Endpoint").up().build();
+    }
+    
+    
+    
+}
